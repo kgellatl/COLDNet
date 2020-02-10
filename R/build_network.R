401e1134ff57061bd028d303b8fd9463038a3388
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2020-02-10 15:33:15 -0500

    documentation

---
+++ R/build_network.R
@@ -0,0 +1,14 @@
+#' build_network
+#'
+#' This function will build a igraph network from a dataframe and calculate the layout.
+#'
+#' @param input the input dataframe
+#' @export
+#' @details
+#' #
+#' @examples
+#'
+
+build_network <- function() {
+  print("Hello, world!")
+}
