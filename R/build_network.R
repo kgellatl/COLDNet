06d7ff23211dfd2afc0c9041dc89db27ada81351
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2020-02-10 15:50:26 -0500

    commit1

--- R/build_network.R
+++ R/build_network.R
@@ -9,6 +9,17 @@
 #' @examples
 #'
 
-build_network <- function() {
-  print("Hello, world!")
+build_network <- function(input) {
+  gene_net <- graph_from_data_frame(Results_from_test_matrix_2000)
+  gene_net <- as.undirected(gene_net)
+
+  E(gene_net)$rho <- Results_from_test_matrix_2000$rho
+  E(gene_net)$pval <- Results_from_test_matrix_2000$p_Bonferroni
+
+  gene_net
+  names(V(gene_net))
+  length(V(gene_net))
+  length(E(gene_net))
+
+  l <- layout_with_fr(gene_net)
 }
