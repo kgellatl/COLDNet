104c2d84b9ddef515b784cf7183c30cfd1a41483
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2020-02-13 02:21:41 -0500

    helper function

---
+++ R/make_visnet.R
@@ -0,0 +1,33 @@
+#' make_visnet
+#'
+#' This function will build a visnetwork object
+#'
+#' @param input_igraph the ggraph plot
+#' @export
+#' @details
+#' #
+#' @examples
+#'
+
+make_visnet <- function(input_igraph, edge_scale = 7){
+  test_net_data_frame <- as_data_frame(input_igraph)
+  nodes <- igraph::as_data_frame(input_igraph, what = "vertices")
+  colnames(nodes) <- "id"
+  nodes$label <- nodes$id
+  edges <- igraph::as_data_frame(input_igraph, what = "edges")
+  edges$width <-  test_net_data_frame$width*edge_scale
+  edges$color <- test_net_data_frame$color
+  vis_obj <- visNetwork(nodes, edges) %>%
+    # Get same layout??
+    # https://stackoverflow.com/questions/47630378/how-to-specify-nodes-positions-in-visnetwork-package-in-r
+    #
+    visIgraphLayout("layout_with_fr") %>%
+    visExport() %>%
+    visOptions(height = "1000px", highlightNearest = TRUE,
+               nodesIdSelection = list(enabled = TRUE))
+  results_make_vis <- vector(mode = "list", length = 3)
+  results_make_vis[[1]] <- vis_obj
+  results_make_vis[[2]] <- edges
+  results_make_vis[[3]] <- nodes
+  return(results_make_vis)
+}
