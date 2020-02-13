aee72d603af482493c43afecfba512b7e6b4a3a4
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2020-02-13 04:09:30 -0500

    separate graphing function. Can be used in shiny??

---
+++ R/plot_ggraph.R
@@ -0,0 +1,49 @@
+#' build_network
+#'
+#' This function will build a igraph network from a dataframe and calculate the layout.
+#'
+#' @param input the input dataframe
+#' @export
+#' @details
+#' #
+#' @examples
+#'
+
+plot_ggraph <- function(input, color, width, col_lab = NULL, width_lab = NULL, scale = "diverge", extract = T){
+
+  if(is.null(col_lab)){
+    col_lab <- color
+  }
+
+  if(is.null(width_lab)){
+    width_lab <- width
+  }
+
+
+  E(input)$edge_color <- as_data_frame(input)[,color]
+  E(input)$edge_weight <- as_data_frame(input)[,width]
+
+  l <- matrix(c(V(input)$x_pos, V(input)$y_pos), ncol = 2)
+
+  g <- ggraph(input, layout = l)
+    g <-  g + geom_edge_link(aes(colour = edge_color, width = edge_weight))
+    g <- g + geom_node_point()
+    if(scale == "diverge"){
+      g <- g + scale_edge_colour_gradient2(name = col_lab)
+    }
+  if(scale == "continuous"){
+    g <- g + scale_edge_colour_gradient(name = col_lab, low = "blue", high = "red")
+  }
+  g <- g + scale_edge_width(name = width_lab, range = c(0.5,3))
+    g <- g + geom_node_text(label = names(V(input)), size = 2)
+    g <- g + theme_void()
+
+    plot(g)
+
+    if(extract){
+      input <- extract_ggraph(input, g)
+    }
+
+    return(input)
+
+}
