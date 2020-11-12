aaebb3e357d81cd4145feb933cd9f894fbcbe8af
KYLE GELLATLY <kyle.gellatly@umassmed.edu>
2020-11-12 16:16:22 -0500

    data.... again

--- R/build_network.R
+++ R/build_network.R
@@ -10,7 +10,6 @@
 #' test_net <- build_network(input = test_net_dat)
 
 build_network <- function(input) {
-
   gene_net <- graph_from_data_frame(input)
   l <- layout_nicely(gene_net)
   V(gene_net)$x_pos <- l[,1]
